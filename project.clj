e652c7e36c360a2bf531bb73283dafdefc261e29
David Jacot <dajac@Davids-MacBook-Pro.local>
2013-05-01 17:17:38 +0200

    back to snapshot

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject react "0.0.1"
+(defproject react "0.0.2-SNAPSHOT"
   :description "Reactive programming in Clojure"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
